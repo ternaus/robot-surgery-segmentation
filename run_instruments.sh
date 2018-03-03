5877a431881c44802d86acabd21259c0824cdfcc
Vladimir Iglovikov <iglovikov@gmail.com>
2018-03-03 13:18:58 -0800

    Deleted helper files

--- run_instruments.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in 0 1 2 3
-do
-   python train.py --device-ids 0,1,2,3 --batch-size 12 --fold $i --workers 12 --lr 0.0001 --n-epochs 10 --type instruments --jaccard-weight 1 --model LinkNet34
-   python train.py --device-ids 0,1,2,3 --batch-size 12 --fold $i --workers 12 --lr 0.00001 --n-epochs 20 --type instruments --jaccard-weight 1 --model LinkNet34
-done
